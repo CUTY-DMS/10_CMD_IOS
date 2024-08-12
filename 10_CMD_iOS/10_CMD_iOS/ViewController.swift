e3807f62adee971528e319b69a91ed25250ad0b2
hxxchxx <a01068166050@gmail.com>
2024-08-12 19:50:35 +0900

    🗂 :: 프로젝트 세팅

---
+++ 10_CMD_iOS/10_CMD_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  10_CMD_iOS
+//
+//  Created by 손희찬 on 8/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
